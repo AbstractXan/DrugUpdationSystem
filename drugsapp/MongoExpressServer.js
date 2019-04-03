201f9c3c41000a4e7a01d052dee506ad6ceff0fc
cs16b004 <42582250+cs16b004@users.noreply.github.com>
2019-04-03 12:35:55 +0530

    Bug in addition Removed

--- drugsapp/MongoExpressServer.js
+++ drugsapp/MongoExpressServer.js
@@ -37,25 +37,34 @@ app.get('/api/find', function(req, res)
       //console.log(fin_result);
     })
 
-})
-app.put('/add',function(req,res)
+})app.put('/api/add',function(req,res)
   {
+    var mech_d = req.query.mechanismOfAction;
+    var eff_d = req.query.adverseEffects;
+    var intrt_d = req.query.interactions;
+    var use_d = req.query.uses;
+    console.log(mech_d,eff_d,intrt_d,use_d);
+    var mech = mech_d.split(":");
+    var effect = eff_d.split(":");
+    var inter= intrt_d.split(":");
+    var use = use_d.split(":");
     var query = {
       article: req.query.article,
       name : req.query.name,
       scientificName: req.query.scientificName,
       class: req.query.class,
-      mechanismOfAction: req.query.mechanismOfAction,
-      adverseEffects: req.query.adverseffects,
-      interactions: req.query.interactions,
-      uses: req.query.uses,
+      mechanismOfAction: mech,
+      adverseEffects: effect,
+      interactions: inter,
+      uses: use,
       date: req.query.date
 
     }
-    dbo.collection("test").insertOne(query,function(err,result){
+    dbo.collection("drugs").insertOne(query,function(err,result){
       if (err) return res.json({message: "Could Not Add"})
       return res.json({message:"Added Successfully"})
     })
   })
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
